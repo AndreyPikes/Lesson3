7793b3422f97eea5c96f50307dd161438bc954c5
Андрей Щукин <1929999@gmail.com>
2021-09-12 17:52:40 +0300

    Downloaded version

---
+++ SApp01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp01
+{
+    class Program
+    {
+
+
+        static bool Sample2(out int a)
+        {
+            a = 1;
+            return true;
+        }
+
+        static void Sample(ref int x, ref int y, ref int z, string s)
+        {
+            // Process ...
+            x = 1;
+            y = 0;
+            z = 2;
+        }
+        
+        static void Main(string[] args)
+        {
+            object o;
+            int a =1;
+            int b = -5;
+            int c = 5;
+            string str = "Hello!";
+            string str2 = (string)str.Clone();
+            string str3 = string.Copy(str);
+            str2 = str;
+
+            Sample(ref a, ref b, ref c, string.Copy(str));
+
+
+            Console.Write("Введите число: ");
+
+            //int number = int.Parse(Console.ReadLine());
+            int i;
+
+            if (int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine($"Вы ввели чило {i}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели некорректное число.");
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SApp03/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp03
+{
+
+    /// <summary>
+    /// Комплексное число
+    /// </summary>
+    class Complex
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// Действительная часть комплексного числа
+        /// </summary>
+        private double re;
+
+        /// <summary>
+        /// Мнимая чатсь комплексного числа
+        /// </summary>
+        private double im { get; set; } // private
+
+        #endregion
+
+        #region Public Properties
+
+        public double Re
+        {
+            get
+            {
+                return re;
+            }
+
+            set
+            {
+                //if (value == 0)
+                //{
+                //    throw new Exception("На ноль делить нельзя!");
+                //}
+                re = value;
+            }
+        }
+
+
+        #endregion
+
+        #region Constructors
+
+        public Complex()
+        {
+
+        }
+
+        public Complex(double re, double im)
+        {
+            if (im == 0)
+            {
+                throw new Exception("На ноль делить нельзя!");
+            }
+            this.re = re;
+            this.im = im;
+        }
+
+        #endregion
+
+        public void SetIm(double im)
+        {
+            this.im = im;
+        }
+
+        public double GetIm()
+        {
+            return im;
+        }
+
+        public Complex Plus(Complex x)
+        {
+            Complex y = new Complex(re + x.re, im + x.im);
+            return y;
+        }
+
+        public Complex Minus(Complex x)
+        {
+            return new Complex(re - x.re, im - x.im);
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"{re} + {im}i";
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complex01 = new Complex(5, 0);
+            //complex01.SetIm(2);
+            complex01.Re = 0;
+            Console.WriteLine(complex01.Re);
+
+            complex01 = new Complex(1, 1);
+            //complex01.re = 5;
+            //complex01.im = 0;
+        }
+    }
+}
